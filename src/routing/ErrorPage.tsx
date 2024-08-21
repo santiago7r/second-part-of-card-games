95508d3e70fccf7f89fb4458e8de2b0d1c33cf0f
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-21 12:02:46 -0500

    Error page component

---
+++ src/routing/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>Oops...</h1>
+      {
+      isRouteErrorResponse(error)
+      ? 'Invalid page'
+      : 'Unexpected error'
+     }
+    </>
+  );
+};
+
+export default ErrorPage;

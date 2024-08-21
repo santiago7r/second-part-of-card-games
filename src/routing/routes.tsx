24d60db420e41ac91878b986fd7f33cf1d559409
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-21 12:04:31 -0500

    Implement dinamyc error

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import GameDetailPage from "../components/GameDetailPage";
+import ErrorPage from "./ErrorPage";
 import Layout from "./Layout";
 
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',

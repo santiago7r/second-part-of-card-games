a5f376d8229e2df1cb4d9df87ff300605d26949e
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-21 12:10:05 -0500

    Add NavBar to error page

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,16 +1,19 @@
+import { Heading } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
   const error = useRouteError();
 
   return (
     <>
-      <h1>Oops...</h1>
+      <NavBar />
+      <Heading>Oops...</Heading>
       {
       isRouteErrorResponse(error)
       ? 'Invalid page'
       : 'Unexpected error'
-     }
+      }
     </>
   );
 };

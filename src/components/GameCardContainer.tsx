88cc15b6644721ba9144cb8e3dd7ebb3d871e14b
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-02 17:56:38 -0500

    scale animation for cards

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,13 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, keyframes } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
 }
 
+const animation = keyframes`
+  from {
+    scale: 1
+  }
+
+  to {
+    scale: 1.05
+  }
+`
+
 const GameCardContainer = ({ children }: Props) => {
+  const myAmazingAnimation = `${animation} infinite 0.5s`
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box 
+      _hover={{
+        animation: myAmazingAnimation
+      }}
+
+      borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );

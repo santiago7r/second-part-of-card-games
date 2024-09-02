d3110bb08a27e4611a2f0ea0d854b81dc24869c2
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-02 18:52:27 -0500

    Roll back details for game page

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -1,13 +1,7 @@
-import { Game } from "../hooks/useGames";
 
 
-
-interface Props {
-    game: Game
-}
-
-const GameDetailPage = ({ game }: Props) => {
-    return <h1 style={{color: "red"}}>{game.name}</h1>
+const GameDetailPage = () => {
+    return <h1 style={{color: "red"}}>GameDetailPage</h1>
 }
 
 export default GameDetailPage;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -10,7 +10,6 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
-  description: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;

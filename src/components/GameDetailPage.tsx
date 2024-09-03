1be086deaee7a7905ddbb9dc225a0b7003213179
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-03 16:10:04 -0500

    Show details when click game name

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -1,7 +1,23 @@
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
 
 
 const GameDetailPage = () => {
-    return <h1 style={{color: "red"}}>GameDetailPage</h1>
+    const {slug} = useParams();
+    const {data: game, isLoading, error} = useGame(slug!);
+
+    if (isLoading) return <Spinner />;
+
+    if(error || !game) throw error;
+
+    return (
+        <>
+            <Heading>{game.name} </Heading>
+            <Text>{game.description_raw}</Text>
+
+        </>
+    )
 }
 
 export default GameDetailPage;

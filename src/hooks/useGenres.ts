57ebaf09bd301125e25ec18cf309f555c0a84d51
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-03 16:41:46 -0500

    unsaved file

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
-import { Genre } from "./Genre";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>('/genres')
 
